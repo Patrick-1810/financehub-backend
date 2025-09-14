4d78d790f62e775bea13def07244d22e733c861a
Patrick-1810 <patrickprestes977@gmail.com>
2025-09-14 09:54:12 -0300

    first commit

---
+++ src/main/java/com/example/financehub/FinancehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.financehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financehub/FinancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
