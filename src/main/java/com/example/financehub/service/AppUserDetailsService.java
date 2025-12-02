b0f0b20729dc0607e05c35a492b84e15a0d7dc19
Patrick-1810 <patrickprestes977@gmail.com>
2025-12-02 11:03:43 -0300

    refactor: remove unused ProfileService dependency from AppUserDetailsService

--- src/main/java/com/example/financehub/service/AppUserDetailsService.java
+++ src/main/java/com/example/financehub/service/AppUserDetailsService.java
@@ -14,19 +14,17 @@ import java.util.Collections;
 @Service
 @RequiredArgsConstructor
 public class AppUserDetailsService implements UserDetailsService {
-    private final ProfileService profileService;
     private final ProfileRepository profileRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-       ProfileEntity existingProfile = profileRepository.findByEmail(email)
-                 .orElseThrow(() -> new UsernameNotFoundException("Profile  not found with email: " +email));
+        ProfileEntity existingProfile = profileRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Profile  not found with email: " +email));
 
-       return User.builder()
-               .username(existingProfile.getEmail())
-               .password(existingProfile.getPassword())
-               .authorities(Collections.emptyList())
-               .build();
+        return User.builder()
+                .username(existingProfile.getEmail())
+                .password(existingProfile.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
     }
-
 }
